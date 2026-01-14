4f2fffbca1d83f57e6cc51cef5b1d68c09ea5f96
ctgold <ctgold@users.noreply.github.com>
2026-01-14 21:38:16 +0800

    Initial commit: Add React TypeScript project with Vite

---
+++ src/AppRouter.tsx
@@ -0,0 +1,19 @@
+import { Routes, Route, Navigate } from 'react-router-dom'
+import LangLayout from './layouts/LangLayout'
+import PublicLayout from './layouts/PublicLayout'
+import HomePage from './pages/HomePage'
+
+export default function AppRouter() {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/en/public" replace />} />
+      <Route path="/:lang/public" element={
+        <LangLayout>
+          <PublicLayout>
+            <HomePage />
+          </PublicLayout>
+        </LangLayout>
+      } />
+    </Routes>
+  )
+}
